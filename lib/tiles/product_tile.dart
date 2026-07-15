8a622962fb970a5b655891730f53e9e18fc10162
Anderson Boeing <anderboeing@gmail.com>
2026-07-15 15:41:18 -0300

    grade e lista de produtos

---
+++ lib/tiles/product_tile.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/datas/product_data.dart';
+
+class ProductTile extends StatelessWidget {
+  const ProductTile(this.type, this.data, {super.key});
+
+  final String type;
+  final ProductData data;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
