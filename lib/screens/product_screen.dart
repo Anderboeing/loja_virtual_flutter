26109c002b88951495490d661cba87e16da5c184
Anderson Boeing <anderboeing@gmail.com>
2026-07-16 13:08:19 -0300

    tela de produto com slider

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/datas/product_data.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class ProductScreen extends StatefulWidget {
   const ProductScreen(this.product, {super.key});
@@ -17,6 +18,49 @@ class _ProductScreenState extends State<ProductScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final Color primaryColor = Theme.of(context).primaryColor;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(product.title),
+        centerTitle: true,
+        backgroundColor: primaryColor,
+        titleTextStyle: TextStyle(color: Colors.white, fontSize: 24.0),
+      ),
+      body: ListView(
+        children: [
+          AspectRatio(
+            aspectRatio: 1.0,
+            child: CarouselSlider(
+              items: product.images.map((url) {
+                return Image.network(url, fit: BoxFit.cover);
+              }).toList(),
+              options: CarouselOptions(),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  product.title,
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500),
+                  maxLines: 3,
+                ),
+                Text(
+                  "R\$ ${product.price.toStringAsFixed(2)}",
+                  style: TextStyle(
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.bold,
+                    color: primaryColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
